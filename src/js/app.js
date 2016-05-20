c79e553b813ea1bd78b0c6ba83d62b6ef2240deb
Christopher Anderson <Christopher@ImTheC.com>
2016-05-20 09:41:24 -0500

    Change file structure

---
+++ src/js/app.js
@@ -0,0 +1,56 @@
+$(function(){
+
+var galleryPopped = true;
+var aboutPopped = false;
+var contactPopped = false;
+
+var paintings = [
+  {name: "Vantage",
+  src: "vantage.png"},
+  {name: "Centric Depths",
+  src: "centric_depths.png"},
+  {name: "Centric Heights",
+  src: "centric_heights.png"},
+  {name: "Day Blush",
+  src: "day_blush.png"},
+  {name: "In The Deep",
+  src: "in_the_deep.png"},
+  {name: "Intercosmic Flow",
+  src: "intercosmic_flow.png"},
+  {name: "Night Lapse",
+  src: "night_lapse_01.png"},
+  {name: "Untitled 03",
+  src: "untitled_03.jpg"},
+  {name: "Night Wind",
+  src: "night_wind.png"},
+  {name: "Northern Nights",
+  src: "northern_nights.jpg"},
+  {name: "White Black 2x2",
+  src: "white_black_2by2.jpg"},
+  {name: "Untitled 00",
+  src: "untitled_00.png"},
+  {name: "Untitled 04",
+  src: "untitled_04.jpg"},
+  {name: "Alexa",
+  src: "alexa.jpg"},
+  {name: "By The Blood",
+  src: "by_the_blood.jpg"},
+  {name: "Sky Birch Field",
+  src: "sky_birch_field.jpg"},
+  {name: "Untitled 02",
+  src: "untitled_02.jpg"},
+  {name: "Untitled 01",
+  src: "untitled_01.jpg"},
+  {name: "Material Love",
+  src: "material_love.jpg"}
+];
+
+  $('body').removeClass('fade-out');
+
+  paintings.forEach(function(item){
+      var art = "<a href='src/images/paintings/" + item.src + "' data-lightbox='gallery'><img class='thumb' data-pin-url='http://www.ArtistChrista.com' data-pin-media='http://www.ArtistChrista.com/src/images/paintings/" + item.src + "' data-pin-description='" + item.name + ". By Christa Anderson.' alt='Thumbnail of " + item.name + ". By Christa Anderson.' src='src/images/thumb/thumb_" + item.src + "'></a>";
+
+      $('.galleryBox').append(art);
+  });
+
+});
